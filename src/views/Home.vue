cfba4da3f7a2ba6f2c5a7337bc73051fc3ff2380
郑先生 <1160206165@qq.com>
2022-08-01 12:15:24 +0800

    feat: 安装 element-plus 组件库

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <el-button type="primary">按钮</el-button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 })
 </script>
