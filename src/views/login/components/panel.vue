99b1f7b1d3d74f0510767975f155bee1e5da0686
郑先生 <1160206165@qq.com>
2022-08-03 10:08:49 +0800

    feat: 结构样式搭建 完成

---
+++ src/views/login/components/panel.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class='panel-container'>
+    <el-tabs type="border-card" stretch style="width: 300px">
+      <el-tab-pane label="账号登录">
+        <el-form :model="usernameForm">
+          <el-form-item label="账号" label-width="60px">
+            <el-input v-model="usernameForm.username" />
+          </el-form-item>
+          <el-form-item label="密码" label-width="60px">
+            <el-input v-model="usernameForm.password" />
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
+      <el-tab-pane label="手机登录">
+        <el-form :model="phoneForm" label-width="120px">
+          <el-form-item label="手机号" label-width="60px">
+            <el-input v-model="phoneForm.username" />
+          </el-form-item>
+          <el-form-item label="验证码" label-width="60px">
+            <div class="code-box">
+              <el-input v-model="phoneForm.password" />
+              <el-button class="btn" type="primary">获取验证码</el-button>
+            </div>
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+export default {
+  name: 'Panel',
+  setup() {
+    const usernameForm = reactive({
+      username: '',
+      password: ''
+    })
+
+    const phoneForm = reactive({
+      username: '',
+      password: ''
+    })
+
+    return {
+      usernameForm,
+      phoneForm
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.code-box {
+  display: flex;
+  .btn {
+    margin-left: 10px;
+  }
+}
+</style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,72 @@
 <template>
-  <div class='login-container'>login</div>
+  <div class='login-container'>
+    <div class="main">
+      <h1 class="title">后台管理系统</h1>
+      <Panel></Panel>
+      <div class="password-options">
+        <div class="left-box">
+          <div class="icon"></div>
+          <span class="text">记住密码</span>
+        </div>
+        <div class="right-box">
+          <span class="text">忘记密码</span>
+        </div>
+      </div>
+      <el-button class="btn" type="primary">立即登录</el-button>
+    </div>
+  </div>
 </template>
 <script>
+import Panel from './components/panel.vue'
 export default {
-  name: 'Login'
+  name: 'Login',
+  components: {
+    Panel
+  }
 }
 </script>
-<style lang='scss' scoped></style>
+<style lang='scss' scoped>
+.login-container {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #b81324;
+  .main {
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: #fff;
+    border-radius: 10px;
+  }
+  .title {
+  }
+
+  .password-options {
+    width: 100%;
+    margin: 10px 0;
+    display: flex;
+    justify-content: space-between;
+    color: #409dff;
+    .left-box {
+      .icon {
+      }
+
+      .text {
+      }
+    }
+
+    .right-box {
+      .text {
+      }
+    }
+  }
+
+  .btn {
+    width: 100%;
+    height: 40px;
+  }
+}
+</style>
