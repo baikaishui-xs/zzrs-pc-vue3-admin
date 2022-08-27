646c892f7edaae01c7c3b15b7c1e42099b9fa1dc
郑先生 <1160206165@qq.com>
2022-08-27 21:08:48 +0800

    feat: 控制有无 token 时的路由访问

--- src/utils/permission.ts
+++ src/utils/permission.ts
@@ -3,12 +3,19 @@
 import router from '@/router'
 import nprogress from 'nprogress'
 import 'nprogress/nprogress.css'
+import store from '@/store'
+
+const whiteList = ['/login', '/404', '/demo'] // 白名单
 
 // 前置守卫
 router.beforeEach((to, from, next) => {
   nprogress.configure({showSpinner: false}) // 是否显示转轮
   nprogress.start() // 开启进度条
-  next()
+  if (store.state.user.token || whiteList.includes(to.path)) {
+    next()
+  } else {
+    next('/login')
+  }
   nprogress.done() // 该方法放在前置守卫的最下面。解决手动切换地址时，进度条不关闭的问题
 })
 
